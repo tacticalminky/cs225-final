1a6d628e86fc9cc2f674eaa81176b4e314ec43bb
tacticalminky <mink91094@gmail.com>
2022-12-10 12:15:29 -0600

    edited docs and added user input

--- README.md
+++ README.md
@@ -6,4 +6,36 @@ We aim to create a graph from [Anime Recommendations Database](https://www.kaggl
 
 ## Instructions
 
+### Main program
+
+1. Make the build directory and create the make file
+
+    ```bash
+    mkdir build
+    cd build
+    cmake ..
+    ```
+
+2. Run the make command to compile the program and then run the program.
+
+    ```bash
+    make main
+    ./main
+    ```
+
+### Graphical output
+
+1. Install Dependencies
+
+    ```bash
+    ```
+
 ## Organization
+
+Main C++ file is in the `entry` directory
+
+C++ header and source files are in the `src` directory
+
+Test source file is in the `tests` directory
+
+Our data sets and python scripts are in the `data` directory
--- entry/main.cpp
+++ entry/main.cpp
@@ -1,5 +1,6 @@
-#include "anime_graph.h"
 #include <iostream>
+
+#include "anime_graph.h"
 
 int main() {
     AnimeGraph graph;
@@ -7,5 +8,43 @@ int main() {
     // graph.makeGraph("../tests/data/test_anime.csv", "../tests/data/test_rating.csv");
 
     graph.writeToCSV();
+
+    std::string res;
+    std::cout << "Would you like some recommendations? (y/n) ";
+    std::cin >> res;
+    if (res == "y" || res == "yes") {
+        Node node;
+        std::cout << "Please input some of the following information" << std::endl;
+
+        std::cout << "ID: ";
+        std::cin >> res;
+        node.id = std::stoi(res);
+
+        std::cout << "Name: ";
+        std::cin >> node.name;
+
+        std::cout << "Generes: ";
+        std::cin >> res;
+        
+        std::cout << "Episodes: ";
+        std::cin >> res;
+        node.episodes = std::stoi(res);
+        
+        std::cout << "Rating: ";
+        std::cin >> res;
+        node.rating = std::stoi(res);
+        
+        std::cout << "Members: ";
+        std::cin >> res;
+        node.members = std::stoi(res);
+
+        std::vector<std::string> recommendations = graph.findTop10Related(node);
+
+        std::cout << "We recommend: " << std::endl;
+        for (unsigned i = 0; i < recommendations.size(); ++i) {
+            std::cout << i + 1 << ": " << recommendations.at(i) << std::endl;
+        }
+    }
+
     return 0;
 }
